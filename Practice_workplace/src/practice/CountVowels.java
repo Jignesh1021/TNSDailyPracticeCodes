d47c4d304b8bbc24ac4b58f6d30bcfb87b527938
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-13 23:43:37 +0530

    Added Count Vowel Code

---
+++ Practice_workplace/src/practice/CountVowels.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class CountVowels {
+
+    public static void main(String[] args) {
+
+        String str = "Programming";
+        int count = 0;
+
+        for (char c : str.toLowerCase().toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Vowels: " + count);
+    }
+}
