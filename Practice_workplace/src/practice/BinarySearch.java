0a10b6bc378f0fca3bd4e48a431d0e3d5514d1c3
More Jignesh Satish <morejignesh67@gmail.com>
2026-04-16 23:37:05 +0530

    Added binart search Code

---
+++ Practice_workplace/src/practice/BinarySearch.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 10, 20, 30, 40, 50 };
+        int target = 30;
+
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Found at index: " + mid);
+                return;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println("Not found");
+    }
+}
